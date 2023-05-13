4b867ca9ab87ce955184ee24eddd33c7314f431d
Your Name <you@example.com>
2023-05-13 21:26:12 +0200

    running js comments

--- playground/node-example.js
+++ playground/node-example.js
@@ -1,1 +1,2 @@
 console.log('IM from node');
+// run this from the terminal by using node node-example.js
---
+++ playground/some.js
@@ -0,0 +1,4 @@
+console.log('hello from another file');
+
+const para = document.querySelector('p');
+console.log(para);
