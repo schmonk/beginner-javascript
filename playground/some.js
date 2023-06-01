b5a6bea10aea0ec5c8c73a1878b5a6f25c070c41
Your Name <you@example.com>
2023-06-01 13:36:22 +0200

    types notes

--- playground/some.js
+++ playground/some.js
@@ -1,4 +1,30 @@
-console.log('hello from another file');
+/* let dog;
 
-const para = document.querySelector('p');
-console.log(para);
+console.log(dog);
+
+let someThingUndefined;
+
+const somethingNull = null;
+
+const cher = {
+  firstName: 'cher',
+};
+
+const teller = {
+  first: 'Raymond',
+  last: 'Teller',
+};
+
+teller.first = 'Teller';
+teller.last = null; //this magician changed his name to be just one word
+ */
+
+let isDrawing = false;
+
+let age = 19;
+
+const ofAge = age > 18;
+console.log(ofAge);
+
+age = 100;
+let age2 = 100;
