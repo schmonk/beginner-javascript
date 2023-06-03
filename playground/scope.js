b7d0d32768d298ae1e9aea81a3eb8c3f807095fb
Your Name <you@example.com>
2023-06-03 11:04:13 +0200

    scope notes

---
+++ playground/scope.js
@@ -0,0 +1,46 @@
+/* const age = 100;
+
+function go() {
+  const myAge = 222; // shadowing a variable. legal, but not allowed!!1
+  const hair = 'blonde';
+  console.log(hair);
+  console.log(myAge);
+}
+
+go(); */
+
+/* function isCool(name) {
+  let cool;
+  if (name === 'wes') {
+    cool = true;
+  }
+  console.log(cool);
+  return cool;
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+} */
+
+/* const dog = 'snickers';
+
+function logDog() {
+  console.log(dog);
+}
+
+function go() {
+  const dog = 'sunny';
+  logDog();
+}
+
+go();
+ */
+
+function sayHi(name) {
+  function yell() {
+    console.log(name.toUpperCase());
+  }
+  yell();
+}
+
+yell();
