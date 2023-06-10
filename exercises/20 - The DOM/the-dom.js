f30aee3dc0402ea46d8987fc1228cb820faeebe3
Your Name <you@example.com>
2023-06-10 16:22:41 +0200

    elment props and methods notes

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,17 @@
+const  p = document.querySelector('p');
+console.log(p);
+const item2 = document.querySelector('.item2');
+const item2Image = item2.querySelector('img');
+const heading = document.querySelector('h2');
+// heading.textContent = `Eric is amazing`;
+console.dir(heading.textContent);
+console.dir(heading.innerText);
+console.dir(heading.innerHTML);
+console.dir(heading.outerHTML);
+
+const pizzaList = document.querySelector('.pizza');
+// console.dir(pizzaList.textContent);
+
+// pizzaList.textContent = ` ${pizzaList.textContent}🍕`; // slow
+pizzaList.insertAdjacentText("afterbegin",`🍕`);
+pizzaList.insertAdjacentText("beforeend",`🍕`);
