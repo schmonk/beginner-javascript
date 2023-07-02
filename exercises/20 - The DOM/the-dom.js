b726a1c15b81f3a4e04144151bf63f7c5c1c9032
Your Name <you@example.com>
2023-07-02 12:04:35 +0200

    dom classes notes

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,8 +1,9 @@
 /* const  p = document.querySelector('p');
-console.log(p);
+// console.log(p);
 const item2 = document.querySelector('.item2');
 const item2Image = item2.querySelector('img');
 const heading = document.querySelector('h2');
+console.dir(heading);
 // heading.textContent = `Eric is amazing`;
 console.dir(heading.textContent);
 console.dir(heading.innerText);
@@ -10,20 +11,22 @@ console.dir(heading.innerHTML);
 console.dir(heading.outerHTML);
 
 const pizzaList = document.querySelector('.pizza');
-// console.dir(pizzaList.textContent);
 
 // pizzaList.textContent = ` ${pizzaList.textContent}🍕`; // slow
 pizzaList.insertAdjacentText("afterbegin",`🍕`);
 pizzaList.insertAdjacentText("beforeend",`🍕`);
+
+console.dir(pizzaList.textContent);
  */
 
 //Classes!
 const pic = document.querySelector('.nice');
 pic.classList.add('open');
+pic.classList.add('round');
 console.log(pic.classList);
 
 function toggleRound() {
-    pic.classList.toggle('round');
+  pic.classList.toggle('round');
 }
 
 pic.addEventListener('click', toggleRound);
