727a7e011bf14648b2144bc9ab283d1872e7b808
schmonkli <eric@seervision.com>
2023-07-23 20:15:16 +0200

    bubbling and target notes

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -10,16 +10,36 @@ const hooray = () => console.log('hooray');
 butts.addEventListener('click', handleClick);
 coolButton.addEventListener('click', hooray);
 
-const buyButtons = document.querySelectorAll('button.buy');
-
 function handleBuyButtonClick(event) {
+  console.log('clicked a button');
   const button = event.target;
-  /*   console.log(button.textContent);
-  console.log(parseFloat(event.target.dataset.price)); */
-  console.log(event.target);
+  console.log(button.textContent);
   console.log(event.currentTarget);
+  // event.stopPropagation();
 }
+
+const buyButtons = document.querySelectorAll('button.buy');
 
 buyButtons.forEach(function (buyButton) {
   buyButton.addEventListener('click', handleBuyButtonClick);
+});
+
+window.addEventListener(
+  'click',
+  function (event) {
+    console.log('YOU clicked the window');
+    console.log(event.target);
+    console.log(event.type);
+    console.log(event.bubbles);
+
+    // event.stopPropagation();
+  },
+  { capture: true },
+);
+
+const photoElement = document.querySelector('.photo');
+
+photoElement.addEventListener('mouseenter', function (e) {
+  console.log(e.currentTarget);
+  console.log(this);
 });
