a8886b95bf07fc1c659a62810666bb336b1fb038
schmonkli <eric@seervision.com>
2023-07-27 18:39:37 +0200

    tabs

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,27 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(event) {
+  // hide all tabPanels
+  tabPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
+  // mark all tabs as unselected
+  tabButtons.forEach((tab) => {
+    tab.setAttribute('aria-selected', false);
+  });
+  // mark this/the clicked tab as selected
+  event.currentTarget.setAttribute('aria-selected', true);
+  // find associated tabPanel and show it
+  const { id } = event.currentTarget; // destructuring
+  /* METHOD 1 */
+  /*  const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"`);
+  tabPanel.hidden = false;
+  */
+  /* METHOD 2: FIND */
+  const tabPanel = tabPanels.find((panel) => panel.getAttribute('aria-labelledby') === id);
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach((button) => button.addEventListener('click', handleTabClick));
