401d70784da66be623b774c12f08c52495c27c51
schmonkli <eric@seervision.com>
2023-08-20 15:44:23 +0200

    switch from face to sarcastic

--- exercises/55 - Face Detection Censorship/pixelated-face.js
+++ exercises/55 - Face Detection Censorship/pixelated-face.js
@@ -1,1 +1,20 @@
-console.log('works');
+const video = document.querySelector('.webcam');
+const canvas = document.querySelector('.video');
+const ctx = canvas.getContext('2d');
+
+const faceCanvas = document.querySelector('.face');
+const faceCtx = faceCanvas.getContext('2d');
+
+const faceDetector = new window.faceDetector();
+
+// write a function that will populate the users video
+
+async function populateVideo() {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: { width: 1280, height: 720 },
+  });
+  video.srcObject = stream;
+  await video.play();
+}
+
+populateVideo();
