0220ddebebaa463cb92b2d8c9749ab588a2156ea
schmonkli <eric@seervision.com>
2023-09-06 15:41:49 +0200

    slider prototype update

---
+++ exercises/59 - Slider/src/index-prototype.js
@@ -0,0 +1,60 @@
+function Slider(slider) {
+  if (!(slider instanceof Element)) {
+    throw new Error('No slider passed in');
+  }
+
+  // select elements needed for slider
+  this.slides = slider.querySelector('.slides');
+  this.slider = slider;
+  const prevButton = slider.querySelector('.goToPrev');
+  const nextButton = slider.querySelector('.goToNext');
+
+  // when this slider is created, run this function. also called a constructor
+  this.startSlider();
+  this.applyClasses();
+
+  /* Event listeners */
+
+  prevButton.addEventListener('click', () => this.move('back'));
+  nextButton.addEventListener('click', () => this.move());
+}
+
+Slider.prototype.startSlider = function () {
+  this.current = this.slider.querySelector('.current') || this.slides.firstElementChild;
+  this.prev = this.current.previousElementSibling || this.slides.lastElementChild;
+  this.next = this.current.nextElementSibling || this.slides.firstElementChild;
+  // console.log(current, prev, next);
+};
+
+Slider.prototype.applyClasses = function () {
+  this.current.classList.add('current');
+  this.prev.classList.add('prev');
+  this.next.classList.add('next');
+};
+Slider.prototype.move = function (direction) {
+  // first strip all the classes of the current slides
+  const classesToRemove = ['prev', 'current', 'next'];
+  this.prev.classList.remove(...classesToRemove);
+  this.current.classList.remove(...classesToRemove);
+  this.next.classList.remove(...classesToRemove);
+  if (direction === 'back') {
+    // make an array of the new values, and destructure them over and into the new vars
+    [this.prev, this.current, this.next] = [
+      this.prev.previousElementSibling || this.slides.lastElementChild,
+      this.prev,
+      this.current,
+    ];
+  } else {
+    [this.prev, this.current, this.next] = [
+      this.current,
+      this.next,
+      this.next.nextElementSibling || this.slides.firstElementChild,
+    ];
+  }
+  this.applyClasses();
+};
+
+const mySlider = new Slider(document.querySelector('.slider'));
+const dogSlider = new Slider(document.querySelector('.dog-slider'));
+
+console.log(mySlider, dogSlider);
