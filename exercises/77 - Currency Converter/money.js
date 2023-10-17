9da441f27a0a0ad5961215cf12d4dc889783ab58
schmonkli <eric@seervision.com>
2023-10-17 12:31:28 +0200

    currency converter exercise start. had to stop because of api issues

--- exercises/77 - Currency Converter/money.js
+++ exercises/77 - Currency Converter/money.js
@@ -1,3 +1,17 @@
+const fromSelect = document.querySelector('[name="from_currency"]');
+const toSelect = document.querySelector('[name="to_currency"]');
+const endPoint = 'https://api.apilayer.com/exchangerates_data/latest';
+const ratesByBase = {}; // will store all rates
+
+const myHeaders = new Headers();
+const apiKey = 'OcmU1xlxSaMjGFkX91f6TS0dx3I8PJep';
+myHeaders.append('apikey', apiKey);
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders,
+};
+
 const currencies = {
   USD: 'United States Dollar',
   AUD: 'Australian Dollar',
@@ -32,3 +46,62 @@ const currencies = {
   ZAR: 'South African Rand',
   EUR: 'Euro',
 };
+
+function generateOptions(options) {
+  return Object.entries(options)
+    .map(([currencyCode, currencyName]) => {
+      // console.log(currencyCode, currencyName);
+      return `<option value="${currencyCode}">${currencyCode} - ${currencyName}</option>`;
+    })
+    .join('');
+}
+
+async function fetchRates(base = 'CHF') {
+  try {
+    const response = await fetch(`${endPoint}?base=${base}`, requestOptions);
+    const result = await response.json();
+    console.log('result: ', result);
+    return result;
+  } catch (error) {
+    console.log('error: ', error);
+    throw error; // You can choose to handle the error or rethrow it
+  }
+  /*   fetch(`${endPoint}?base=${base}`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => console.log('error: ', error)); */
+}
+
+async function convert(amount, from, to) {
+  // we could fetch the rates each time
+  // but this could be slow
+  // so instead: create baseRates object
+  // first check if we have from rate
+  if (!ratesByBase[from]) {
+    console.log(`Oh no, we dont have ${from} to convert to ${to}. So lets go and get that!`);
+  }
+  const rates = await fetchRates(from);
+  console.log('rates: ', rates);
+  //store for next time
+  ratesByBase[from] = rates;
+}
+
+async function someFunction() {
+  // Call the 'convert' function and await its result
+  try {
+    const convertedAmount = await convert(amount, from, to);
+    console.log('Converted amount: ', convertedAmount);
+  } catch (error) {
+    console.error('Error during conversion:', error);
+  }
+}
+
+// Call someFunction to start the conversion process
+someFunction();
+
+const optionsHTML = generateOptions(currencies);
+//populate options
+fromSelect.innerHTML = optionsHTML;
+toSelect.innerHTML = optionsHTML;
